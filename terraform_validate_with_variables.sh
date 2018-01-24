e220e169cab46dd7e2cdb6b258b311c69036a8e8
Anton Babenko <anton@antonbabenko.com>
2018-01-24 13:34:34 +0100

    Updated scripts

--- terraform_validate_with_variables.sh
+++ terraform_validate_with_variables.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
-for file in "$@"; do
-  terraform validate -check-variables=true "$file"
+declare -a paths
+index=0
+
+for file_with_path in "$@"; do
+  paths[index]=$(dirname "$file_with_path")
+  let "index+=1"
+done
+
+for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
+  pushd "$path_uniq" > /dev/null
+  terraform validate -check-variables=true
+  popd > /dev/null
 done
