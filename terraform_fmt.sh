b0d43204ca5dd994dd0b9f2a4c793ff807cfc303
Anton Babenko <anton@antonbabenko.com>
2018-03-06 13:58:03 +0100

    Format tfvars files explicitely, because terraform fmt ignores them (#9)

--- terraform_fmt.sh
+++ terraform_fmt.sh
@@ -2,10 +2,17 @@
 set -e
 
 declare -a paths
+declare -a tfvars_files
+
 index=0
 
 for file_with_path in "$@"; do
   paths[index]=$(dirname "$file_with_path")
+
+  if [[ "$file_with_path" == *".tfvars" ]]; then
+    tfvars_files+=("$file_with_path")
+  fi
+
   let "index+=1"
 done
 
@@ -13,4 +20,9 @@ for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   pushd "$path_uniq" > /dev/null
   terraform fmt
   popd > /dev/null
+done
+
+# terraform.tfvars are excluded by `terraform fmt`
+for tfvars_file in "${tfvars_files[@]}"; do
+  terraform fmt "$tfvars_file"
 done
