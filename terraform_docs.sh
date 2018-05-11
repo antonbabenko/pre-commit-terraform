11470103425d54a16ce2064358c4125898583098
gabel0287 <gabel0287@gmail.com>
2018-05-11 15:44:31 -0700

    Only replace between markers

--- terraform_docs.sh
+++ terraform_docs.sh
@@ -22,6 +22,7 @@ for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
   pushd "$path_uniq" > /dev/null
-  terraform-docs md ./ > README.md
+  DOCS=$(terraform-docs md ./)
+  perl -0pe 's/(<!-- BEGINNING OF TERRAFORM-DOCS HOOK -->).*(<!-- END OF TERRAFORM-DOCS HOOK -->)/\1\n'"$DOCS"'\n\2/s' README.md
   popd > /dev/null
 done
