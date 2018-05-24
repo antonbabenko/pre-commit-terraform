a5781f3beddfba825d6f19cf70912b8c709d4bc4
Robin Bowes <robin.bowes@yo61.com>
2018-05-24 20:54:57 +0100

    Only run validate if .tf files exist in the directory.

--- terraform_validate_no_variables.sh
+++ terraform_validate_no_variables.sh
@@ -8,19 +8,19 @@ for file_with_path in "$@"; do
   file_with_path="${file_with_path// /__REPLACED__SPACE__}"
 
   paths[index]=$(dirname "$file_with_path")
-  let "index+=1"
+  (( "index+=1" ))
 done
 
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
   pushd "$path_uniq" > /dev/null
-  terraform validate -check-variables=false
-
-  if [[ "$?" -ne 0 ]]; then
-    echo
-    echo "Failed path: $path_uniq"
-    echo "================================"
+  if [[ -n "$(find . -maxdepth 1 -name '*.tf' -print -quit)" ]] ; then
+    if ! terraform validate -check-variables=false ; then
+      echo
+      echo "Failed path: $path_uniq"
+      echo "================================"
+    fi
   fi
   popd > /dev/null
 done
