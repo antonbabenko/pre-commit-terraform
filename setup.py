9aa971c069da7ef66f0a30075e6b68f21f243059
rothandrew <roth.andy@gmail.com>
2018-12-13 22:16:01 -0500

    Add new hook for running terraform-docs with replacing README.md from doc in main.tf

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pre-commit-terraform',
+    description='Pre-commit hooks for Terraform',
+    url='https://github.com/antonbabenko/pre-commit-terraform',
+    version_format='{tag}+{gitsha}',
+
+    author='Andrew Roth',
+    author_email='roth.andy@gmail.com',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    packages=find_packages(exclude=('tests*', 'testing*')),
+    install_requires=[
+        'setuptools-git-version',
+    ],
+    entry_points={
+        'console_scripts': [
+            'terraform_docs_replace = pre_commit_hooks.terraform_docs_replace:main',
+        ],
+    },
+)
