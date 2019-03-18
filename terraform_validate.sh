a34bebcdd09ade6163c9e19176dc8cf2541176e8
Robin Bowes <robin.bowes@yo61.com>
2019-03-18 13:42:17 +0000

    Use more idiomatic bash code

--- terraform_validate.sh
+++ terraform_validate.sh
@@ -1,30 +1,64 @@
 #!/usr/bin/env bash
-set -e
 
-declare -a paths
-index=0
-error=0
+set -euo pipefail
 
-for file_with_path in "$@"; do
-  file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+# global vars
+declare -A dirs_to_test
+ERROR=0
 
-  paths[index]=$(dirname "$file_with_path")
-  (( "index+=1" ))
-done
+# main code entrypoint
+main() {
+  get_uniq_dirs "$@"
+  process_dirs
+  exit "$ERROR"
+}
 
-for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+# reduce the command line args to a list of valid, uniq dirs
+get_uniq_dirs() {
+  local file
+  local abs_file
+  local abs_dir
+  for file in "$@" ; do
+    # Check that file exists
+    [[ -e $file ]] || continue
 
-  if [[ -n "$(find . -maxdepth 1 -name '*.tf' -print -quit)" ]] ; then
-    if ! terraform validate $path_uniq; then
-      error=1
-      echo
-      echo "Failed path: $path_uniq"
-      echo "================================"
+    # get absolute path
+    abs_file="$(realpath "$file")"
+
+    # test it's a dir
+    if [[ -d $abs_file ]] ; then
+      dirs_to_test["$abs_file"]=1
+      continue
     fi
-  fi
-done
 
-if [[ "${error}" -ne 0 ]] ; then
-  exit 1
-fi
+    # treat as a file
+    abs_dir="$(dirname "$abs_file")"
+    dirs_to_test["$abs_dir"]=1
+  done
+}
+  
+# validate one directory
+test_dir() {
+  local dir="$1" ; shift
+  (
+    cd "$dir"
+    if stat -t -- *.tf >/dev/null 2>&1 ; then
+      if ! terraform validate -check-variables=true ; then
+        echo
+        echo "Failed path: $dir"
+        echo "================================"
+        return 1
+      fi
+    fi
+  )
+}
+
+# process each directory
+process_dirs() {
+  local dir
+  for dir in "${!dirs_to_test[@]}"; do
+    test_dir "$dir" || ERROR=1
+  done
+}
+
+main "$@"
