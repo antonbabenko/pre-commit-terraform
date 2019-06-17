35e0356188b64a4c5af9a4e7200d936e514cba71
Paweł Szczepaniak <krzyzakp@users.noreply.github.com>
2019-06-17 13:09:31 +0200

    Upgraded to work with Terraform >= 0.12 (#44)

--- terraform_validate.sh
+++ terraform_validate.sh
@@ -3,6 +3,7 @@ set -e
 
 declare -a paths
 index=0
+error=0
 
 for file_with_path in "$@"; do
   file_with_path="${file_with_path// /__REPLACED__SPACE__}"
@@ -14,13 +15,16 @@ done
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
-  pushd "$path_uniq" > /dev/null
   if [[ -n "$(find . -maxdepth 1 -name '*.tf' -print -quit)" ]] ; then
-    if ! terraform validate -check-variables=false ; then
+    if ! terraform validate $path_uniq; then
+      error=1
       echo
       echo "Failed path: $path_uniq"
       echo "================================"
     fi
   fi
-  popd > /dev/null
 done
+
+if [[ "${error}" -ne 0 ]] ; then
+  exit 1
+fi
