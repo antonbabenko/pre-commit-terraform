950df752488ba37e31752fefd838ddc496d85aa3
Guido Dobboletta <guidodobboletta@gmail.com>
2019-09-04 23:49:37 -0500

    black and pep8

--- pre_commit_hooks/terraform_docs_replace.py
+++ pre_commit_hooks/terraform_docs_replace.py
@@ -10,45 +10,41 @@ def main(argv=None):
                        pulling the documentation from main.tf in order to replace the entire
                        README.md file each time."""
     )
+    parser.add_argument("--dest", dest="dest", default="README.md")
+    parser.add_argument("--sort-inputs-by-required", dest="sort", action="store_true")
     parser.add_argument(
-        '--dest', dest='dest', default='README.md',
+        "--with-aggregate-type-defaults", dest="aggregate", action="store_true"
     )
-    parser.add_argument(
-        '--sort-inputs-by-required', dest='sort', action='store_true',
-    )
-    parser.add_argument(
-        '--with-aggregate-type-defaults', dest='aggregate', action='store_true',
-    )
-    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
     args = parser.parse_args(argv)
 
     dirs = []
     for filename in args.filenames:
-        if (os.path.realpath(filename) not in dirs and \
-                (filename.endswith(".tf") or filename.endswith(".tfvars"))):
+        if os.path.realpath(filename) not in dirs and (
+            filename.endswith(".tf") or filename.endswith(".tfvars")
+        ):
             dirs.append(os.path.dirname(filename))
 
     retval = 0
 
-    for dir in dirs:
+    for dir_list in dirs:
         try:
-            procArgs = []
-            procArgs.append('terraform-docs')
+            procargs = ["terraform-docs"]
             if args.sort:
-                procArgs.append('--sort-inputs-by-required')
+                procargs.append("--sort-inputs-by-required")
             if args.aggregate:
-                procArgs.append('--with-aggregate-type-defaults')
-            procArgs.append('md')
-            procArgs.append("./{dir}".format(dir=dir))
-            procArgs.append("| sed -e '$ d' -e 'N;/^\\n$/D;P;D'")
-            procArgs.append('>')
-            procArgs.append("./{dir}/{dest}".format(dir=dir,dest=args.dest))
-            subprocess.check_call(" ".join(procArgs), shell=True)
+                procargs.append("--with-aggregate-type-defaults")
+            procargs.append("md")
+            procargs.append("./{dir}".format(dir=dir_list))
+            procargs.append("| sed -e '$ d' -e 'N;/^\\n$/D;P;D'")
+            procargs.append(">")
+            procargs.append("./{dir}/{dest}".format(dir=dir_list, dest=args.dest))
+            subprocess.check_call(" ".join(procargs), shell=True)
         except subprocess.CalledProcessError as e:
             print(e)
             retval = 1
     return retval
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
