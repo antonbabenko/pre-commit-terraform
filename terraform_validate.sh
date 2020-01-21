2e5f0171455cecc33e914c42a92817d2ea1154e8
Anton Babenko <anton@antonbabenko.com>
2020-01-21 11:51:53 +0100

    Added shfmt to autoformat shell scripts

--- terraform_validate.sh
+++ terraform_validate.sh
@@ -9,13 +9,13 @@ for file_with_path in "$@"; do
   file_with_path="${file_with_path// /__REPLACED__SPACE__}"
 
   paths[index]=$(dirname "$file_with_path")
-  (( "index+=1" ))
+  (("index+=1"))
 done
 
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
-  if [[ -n "$(find . -maxdepth 1 -name '*.tf' -print -quit)" ]] ; then
+  if [[ -n "$(find . -maxdepth 1 -name '*.tf' -print -quit)" ]]; then
     if ! terraform validate $path_uniq; then
       error=1
       echo
@@ -25,6 +25,6 @@ for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   fi
 done
 
-if [[ "${error}" -ne 0 ]] ; then
+if [[ "${error}" -ne 0 ]]; then
   exit 1
 fi
