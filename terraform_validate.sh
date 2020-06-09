78e93aa2ece2a9763d024f54dd9c242996f82ed7
Jeppeks1 <Jeppeks1@hotmail.com>
2020-06-09 22:41:37 +0200

    Debug required changes to validation script

--- terraform_validate.sh
+++ terraform_validate.sh
@@ -12,6 +12,8 @@ for file_with_path in "$@"; do
   (("index+=1"))
 done
 
+echo $paths
+
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
@@ -24,23 +26,38 @@ for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
     # but fall through to the current directory.
     while [[ "$terraform_path" != "." ]]; do
       if [[ -d "$terraform_path/.terraform" ]]; then
+        echo "Break: $terraform_path"
         break
       else
+        echo "Else: $terraform_path"
         terraform_path=$(dirname "$terraform_path")
       fi
     done
 
     validate_path="${path_uniq#"$terraform_path"}"
+    echo "Validated path: ${validate_path}"
 
     # Change to the directory that has been initialized, run validation, then
     # change back to the starting directory.
     cd "$(realpath "$terraform_path")"
+    echo "Init and validate dir: ${pwd}"
+
+    # Set an empty provider block to satisfy the AzureRM provider when running validation
+    # checks, in case the current directory is a module with no provider block.
+    echo "provider \"azurerm\" {features{}}" > temp-provider.tf
+
+    # Initializing the directory before validation is required.
+    # TODO: Init
+
     if ! terraform validate $validate_path; then
       error=1
       echo
       echo "Failed path: $path_uniq"
       echo "================================"
     fi
+
+    # rm temp-provider.tf
+
     cd "$starting_path"
   fi
 done
