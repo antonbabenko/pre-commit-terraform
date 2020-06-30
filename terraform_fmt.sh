33f9f5f219671b618e17e04348ece5f7a9032518
Eric L Frederich <eric.frederich@siemens.com>
2020-06-30 14:25:50 -0400

    Honor excludes; don't run against entire directories (closes #121)

--- terraform_fmt.sh
+++ terraform_fmt.sh
@@ -1,34 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-declare -a paths
-declare -a tfvars_files
-
-index=0
-
 for file_with_path in "$@"; do
   file_with_path="${file_with_path// /__REPLACED__SPACE__}"
-
-  paths[index]=$(dirname "$file_with_path")
-
-  if [[ "$file_with_path" == *".tfvars" ]]; then
-    tfvars_files+=("$file_with_path")
-  fi
-
-  let "index+=1"
-done
-
-for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
-
-  pushd "$path_uniq" > /dev/null
-  terraform fmt
+  pushd $(dirname "$file_with_path") > /dev/null
+  terraform fmt $(basename "$file_with_path")
   popd > /dev/null
-done
-
-# terraform.tfvars are excluded by `terraform fmt`
-for tfvars_file in "${tfvars_files[@]}"; do
-  tfvars_file="${tfvars_file//__REPLACED__SPACE__/ }"
-
-  terraform fmt "$tfvars_file"
 done
