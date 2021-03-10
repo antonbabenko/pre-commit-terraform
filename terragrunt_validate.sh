6922bdad1f23e2e84e34f115130c07e637c0a782
Thomas Meckel <tmeckel@users.noreply.github.com>
2021-03-10 20:33:29 +0100

    * Added parameter --exclude-path to terragrunt_validate.sh * Using now same code structure in terragrunt_validate.sh as terraform_validate.sh

--- terragrunt_validate.sh
+++ terragrunt_validate.sh
@@ -1,23 +1,99 @@
 #!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
-declare -a paths
+main() {
+  initialize_
+  parse_cmdline_ "$@"
+  terragrunt_validate_
+}
 
-index=0
+to_abs_path() {
+    local target="$1"
 
-for file_with_path in "$@"; do
-  file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+    if [ "$target" == "." ]; then
+        echo "$(pwd)"
+    elif [ "$target" == ".." ]; then
+        echo "$(dirname "$(pwd)")"
+    else
+        echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+    fi
+}
 
-  paths[index]=$(dirname "$file_with_path")
+initialize_() {
+  # get directory containing this script
+  local dir
+  local source
+  source="${BASH_SOURCE[0]}"
+  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
+    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  _SCRIPT_DIR="$(dirname "$source")"
 
-  let "index+=1"
-done
+  # source getopt function
+  # shellcheck source=lib_getopt
+  . "$_SCRIPT_DIR/lib_getopt"
+}
 
-for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+parse_cmdline_() {
+  declare argv
+  argv=$(getopt -o x: --long exclude-path: -- "$@") || return
+  eval "set -- $argv"
 
-  pushd "$path_uniq" > /dev/null
-  terragrunt validate
-  popd > /dev/null
-done
+  for argv; do
+    case $argv in
+      -x | --exclude-path)
+        shift
+        EXCLUDED_PATHS+=("$(to_abs_path $1)")
+        shift
+        ;;
+      --)
+        shift
+        FILES=("$@")
+        break
+        ;;
+    esac
+  done
+}
+
+terragrunt_validate_() {
+  declare -a paths
+
+  local index=0
+  local error=0
+  local file_with_path
+
+  for file_with_path in "${FILES[@]}"; do
+    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+    if [[ "${EXCLUDED_PATHS[@]}" =~ "$(dirname "$file_with_path")" ]]; then
+      continue
+    fi
+
+    paths[index]=$(dirname "$file_with_path")
+    ((index += 1))
+  done
+
+  for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
+    path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+
+    pushd "$path_uniq" > /dev/null
+
+    set +e
+    validate_output=$(terragrunt validate 2>&1)
+    validate_code=$?
+    set -e
+
+    if [[ $validate_code != 0 ]]; then
+      error=1
+      echo "Validation failed: $path_uniq"
+      echo "$validate_output"
+      echo
+    fi
+
+    popd > /dev/null
+  done
+}
