366463ca87ff06ead26991907e2c64ef79a94bc5
Javier Collado <javier.collado@tutanota.com>
2021-07-22 00:43:59 +0200

    Do not use pushd/popd when running tflint
    This is get the full path in errors returned from tflint. Otherwise,
    multiple errors might refer to the same file name (for example, main.tf)
    in different directories and it might be very difficult to find the
    exact location of each of the errors reported.

--- terraform_tflint.sh
+++ terraform_tflint.sh
@@ -61,10 +61,7 @@ tflint_() {
 
   for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
     path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
-
-    pushd "$path_uniq" > /dev/null
-    tflint "${ARGS[@]}"
-    popd > /dev/null
+    tflint "${ARGS[@]}" $path_uniq
   done
 }
 
