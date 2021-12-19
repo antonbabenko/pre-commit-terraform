51f8e71727be7bf1fa3824f00c6c2edd514b4a81
Carlos Bustillo <20931458+carlosbustillordguez@users.noreply.github.com>
2021-12-20 00:13:17 +0100

    Improve terrascan hook script
    - Pass to the terrascan command all supplied arguments.
    - Allow inspecting each directory with TF config files for the current
      commit and fetch the final terrascan exit code.
    - Apply linter recommendations.

--- terrascan.sh
+++ terrascan.sh
@@ -4,27 +4,42 @@ set -eo pipefail
 main() {
   initialize_
   parse_cmdline_ "$@"
-
-  # propagate $FILES to custom function
-  terrascan_ "$ARGS" "$FILES"
+  terrascan_
 }
 
 terrascan_() {
   # consume modified files passed from pre-commit so that
   # terrascan runs against only those relevant directories
-  for file_with_path in $FILES; do
+  for file_with_path in "${FILES[@]}"; do
     file_with_path="${file_with_path// /__REPLACED__SPACE__}"
     paths[index]=$(dirname "$file_with_path")
-
-    let "index+=1"
+    ((index++)) || true
   done
 
+  # allow terrascan continue if exit_code greater than 0
+  set +e
+  terrascan_final_exit_code=0
+
+  # for each path run terrascan
   for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
     path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
     pushd "$path_uniq" > /dev/null
-    terrascan scan -i terraform $ARGS
+
+    # pass the arguments to terrascan
+    # shellcheck disable=SC2068 # terrascan fails when is used "${ARGS[@]}"
+    terrascan scan -i terraform ${ARGS[@]}
+
+    local exit_code=$?
+    if [ $exit_code != 0 ]; then
+      terrascan_final_exit_code=$exit_code
+    fi
+
     popd > /dev/null
   done
+
+  # return the terrascan final exit_code
+  set -e
+  exit $terrascan_final_exit_code
 }
 
 initialize_() {
