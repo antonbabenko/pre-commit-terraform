a62dd8f59c720b5b35d2dbf4b958aaff8b161628
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2021-12-22 22:20:11 +0200

    Quick refactoring

--- terraform_checkov.sh
+++ terraform_checkov.sh
@@ -2,41 +2,42 @@
 
 set -eo pipefail
 
-main() {
-  initialize_
-  parse_cmdline_ "$@"
+function main {
+  common::initialize
+  common::parse_cmdline "$@"
   checkov_ "${ARGS[*]}" "${FILES[@]}"
 }
 
-initialize_() {
+function common::initialize {
+  local SCRIPT_DIR
   # get directory containing this script
-  local dir
-  local source
-  source="${BASH_SOURCE[0]}"
-  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
-    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
-    source="$(readlink "$source")"
-    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $source != /* ]] && source="$dir/$source"
-  done
-  _SCRIPT_DIR="$(dirname "$source")"
+  SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
   # source getopt function
   # shellcheck source=lib_getopt
-  . "$_SCRIPT_DIR/lib_getopt"
+  . "$SCRIPT_DIR/lib_getopt"
 }
 
-parse_cmdline_() {
-  declare argv
-  argv=$(getopt -o a: --long args: -- "$@") || return
+# common global arrays.
+# Populated in `parse_cmdline` and can used in hooks functions
+declare -a ARGS=()
+declare -a HOOK_CONFIG=()
+declare -a FILES=()
+function common::parse_cmdline {
+  local argv
+  argv=$(getopt -o a:,h: --long args:,hook-config: -- "$@") || return
   eval "set -- $argv"
 
   for argv; do
     case $argv in
       -a | --args)
         shift
-        expanded_arg="${1//__GIT_WORKING_DIR__/$PWD}"
-        ARGS+=("$expanded_arg")
+        ARGS+=("$1")
+        shift
+        ;;
+      -h | --hook-config)
+        shift
+        HOOK_CONFIG+=("$1;")
         shift
         ;;
       --)
@@ -46,10 +47,9 @@ parse_cmdline_() {
         ;;
     esac
   done
-
 }
 
-checkov_() {
+function checkov_ {
   local -r args="$1"
   shift 1
   local -a -r files=("$@")
@@ -96,9 +96,5 @@ checkov_() {
   # return the checkov final exit_code
   exit $checkov_final_exit_code
 }
-
-# global arrays
-declare -a ARGS
-declare -a FILES
 
 [[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
