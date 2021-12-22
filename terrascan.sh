0e1a9898a0cd01705b61e0908217944a9185ba78
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2021-12-22 22:25:25 +0200

    chore: Refactor terrascan hook

--- terrascan.sh
+++ terrascan.sh
@@ -1,75 +1,30 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-main() {
-  initialize_
-  parse_cmdline_ "$@"
-  terrascan_ "${ARGS[*]}" "${FILES[@]}"
+function main {
+  common::initialize
+  common::parse_cmdline "$@"
+  common::per_dir_hook "${ARGS[*]}" "${FILES[@]}"
 }
 
-terrascan_() {
-  local -r args="${1}"
-  shift 1
-  local -a -r files=("$@")
-
-  # consume modified files passed from pre-commit so that
-  # terrascan runs against only those relevant directories
-  for file_with_path in "${files[@]}"; do
-    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
-    paths[index]=$(dirname "$file_with_path")
-    index=$((index + 1))
-  done
-
-  # allow terrascan to continue if exit_code is greater than 0
-  # preserve errexit status
-  shopt -qo errexit && ERREXIT_IS_SET=true
-  set +e
-  terrascan_final_exit_code=0
-
-  # for each path run terrascan
-  for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-    path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
-    pushd "$path_uniq" > /dev/null
-
-    # pass the arguments to terrascan
-    # shellcheck disable=SC2086 # terrascan fails when quoting is used ("$arg" vs $arg)
-    terrascan scan -i terraform $args
-
-    local exit_code=$?
-    if [ $exit_code != 0 ]; then
-      terrascan_final_exit_code=$exit_code
-    fi
-
-    popd > /dev/null
-  done
-
-  # restore errexit if it was set before the "for" loop
-  [[ $ERREXIT_IS_SET ]] && set -e
-  # return the terrascan final exit_code
-  exit $terrascan_final_exit_code
-}
-
-initialize_() {
+function common::initialize {
+  local SCRIPT_DIR
   # get directory containing this script
-  local dir
-  local source
-  source="${BASH_SOURCE[0]}"
-  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
-    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
-    source="$(readlink "$source")"
-    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $source != /* ]] && source="$dir/$source"
-  done
-  _SCRIPT_DIR="$(dirname "$source")"
+  SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
   # source getopt function
   # shellcheck source=lib_getopt
-  . "$_SCRIPT_DIR/lib_getopt"
+  . "$SCRIPT_DIR/lib_getopt"
 }
 
-parse_cmdline_() {
-  declare argv
-  argv=$(getopt -o a: --long args: -- "$@") || return
+# common global arrays.
+# Populated in `parse_cmdline` and can used in hooks functions
+declare -a ARGS=()
+declare -a HOOK_CONFIG=()
+declare -a FILES=()
+function common::parse_cmdline {
+  local argv
+  argv=$(getopt -o a:,h: --long args:,hook-config: -- "$@") || return
   eval "set -- $argv"
 
   for argv; do
@@ -79,17 +34,74 @@ parse_cmdline_() {
         ARGS+=("$1")
         shift
         ;;
+      -h | --hook-config)
+        shift
+        HOOK_CONFIG+=("$1;")
+        shift
+        ;;
       --)
         shift
-        FILES+=("$@")
+        FILES=("$@")
         break
         ;;
     esac
   done
 }
 
-# global arrays
-declare -a ARGS=()
-declare -a FILES=()
+function common::per_dir_hook {
+  local -r args="$1"
+  shift 1
+  local -a -r files=("$@")
+
+  # consume modified files passed from pre-commit so that
+  # hook runs against only those relevant directories
+  local index=0
+  for file_with_path in "${files[@]}"; do
+    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+    dir_paths[index]=$(dirname "$file_with_path")
+
+    ((index += 1))
+  done
+
+  # allow hook to continue if exit_code is greater than 0
+  # preserve errexit status
+  shopt -qo errexit && ERREXIT_IS_SET=true
+  set +e
+  local final_exit_code=0
+
+  # run hook for each path
+  for path_uniq in $(echo "${dir_paths[*]}" | tr ' ' '\n' | sort -u); do
+    path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+    pushd "$path_uniq" > /dev/null
+
+    per_dir_hook_unique_part "$args"
+
+    local exit_code=$?
+    if [ "$exit_code" != 0 ]; then
+      final_exit_code=$exit_code
+    fi
+
+    popd > /dev/null
+  done
+
+  # restore errexit if it was set before the "for" loop
+  [[ $ERREXIT_IS_SET ]] && set -e
+  # return the hook final exit_code
+  exit $final_exit_code
+}
+
+function per_dir_hook_unique_part {
+  # common logic located in common::per_dir_hook
+  local -r args="$1"
+
+  # pass the arguments to hook
+  # shellcheck disable=SC2068 # hook fails when quoting is used ("$arg[@]")
+  terrascan scan -i terraform ${args[@]}
+
+  # return exit code to common::per_dir_hook
+  local exit_code=$?
+  return $exit_code
+}
 
 [[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
