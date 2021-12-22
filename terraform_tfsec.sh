dea356fc2ac86818aa14a7a047aceddb46250cda
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2021-12-23 00:51:22 +0200

    chore: Refactor tfsec hook

--- terraform_tfsec.sh
+++ terraform_tfsec.sh
@@ -1,74 +1,110 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-main() {
-  initialize_
-  parse_cmdline_ "$@"
-
-  # propagate $FILES to custom function
-  tfsec_ "$ARGS" "${FILES[*]}"
+function main {
+  common::initialize
+  common::parse_cmdline "$@"
+  # Support for setting PATH to repo root.
+  ARGS=${ARGS[*]/__GIT_WORKING_DIR__/$(pwd)\/}
+  common::per_dir_hook "$ARGS" "${FILES[@]}"
 }
 
-tfsec_() {
-  # consume modified files passed from pre-commit so that
-  # tfsec runs against only those relevant directories
-  for file_with_path in ${FILES[*]}; do
-    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
-    paths[index]=$(dirname "$file_with_path")
-
-    let "index+=1"
-  done
-
-  for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-    path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
-    pushd "$path_uniq" > /dev/null
-    tfsec $ARGS
-    popd > /dev/null
-  done
-}
-
-initialize_() {
+function common::initialize {
+  local SCRIPT_DIR
   # get directory containing this script
-  local dir
-  local source
-  source="${BASH_SOURCE[0]}"
-  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
-    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
-    source="$(readlink "$source")"
-    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $source != /* ]] && source="$dir/$source"
-  done
-  _SCRIPT_DIR="$(dirname "$source")"
+  SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
   # source getopt function
   # shellcheck source=lib_getopt
-  . "$_SCRIPT_DIR/lib_getopt"
+  . "$SCRIPT_DIR/lib_getopt"
 }
 
-parse_cmdline_() {
-  declare argv
-  argv=$(getopt -o a: --long args: -- "$@") || return
+# common global arrays.
+# Populated in `parse_cmdline` and can used in hooks functions
+declare -a ARGS=()
+declare -a HOOK_CONFIG=()
+declare -a FILES=()
+function common::parse_cmdline {
+  local argv
+  argv=$(getopt -o a:,h: --long args:,hook-config: -- "$@") || return
   eval "set -- $argv"
 
   for argv; do
     case $argv in
       -a | --args)
         shift
-        expanded_arg="${1//__GIT_WORKING_DIR__/$PWD}"
-        ARGS+=("$expanded_arg")
+        ARGS+=("$1")
+        shift
+        ;;
+      -h | --hook-config)
+        shift
+        HOOK_CONFIG+=("$1;")
         shift
         ;;
       --)
         shift
-        FILES+=("$@")
+        FILES=("$@")
         break
         ;;
     esac
   done
 }
 
-# global arrays
-declare -a ARGS=()
-declare -a FILES=()
+function common::per_dir_hook {
+  local -r args="$1"
+  shift 1
+  local -a -r files=("$@")
+
+  # consume modified files passed from pre-commit so that
+  # hook runs against only those relevant directories
+  local index=0
+  for file_with_path in "${files[@]}"; do
+    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+    dir_paths[index]=$(dirname "$file_with_path")
+
+    ((index += 1))
+  done
+
+  # allow hook to continue if exit_code is greater than 0
+  # preserve errexit status
+  shopt -qo errexit && ERREXIT_IS_SET=true
+  set +e
+  local final_exit_code=0
+
+  # run hook for each path
+  for dir_path in $(echo "${dir_paths[*]}" | tr ' ' '\n' | sort -u); do
+    dir_path="${dir_path//__REPLACED__SPACE__/ }"
+    pushd "$dir_path" > /dev/null
+
+    per_dir_hook_unique_part "$args" "$dir_path"
+
+    local exit_code=$?
+    if [ "$exit_code" != 0 ]; then
+      final_exit_code=$exit_code
+    fi
+
+    popd > /dev/null
+  done
+
+  # restore errexit if it was set before the "for" loop
+  [[ $ERREXIT_IS_SET ]] && set -e
+  # return the hook final exit_code
+  exit $final_exit_code
+}
+
+function per_dir_hook_unique_part {
+  # common logic located in common::per_dir_hook
+  local -r args="$1"
+  local -r dir_path="$2"
+
+  # pass the arguments to hook
+  # shellcheck disable=SC2068 # hook fails when quoting is used ("$arg[@]")
+  tfsec ${args[@]}
+
+  # return exit code to common::per_dir_hook
+  local exit_code=$?
+  return $exit_code
+}
 
 [[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
