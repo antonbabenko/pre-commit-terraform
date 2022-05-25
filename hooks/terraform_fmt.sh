d0c781e98a310b1b03900280feaf630832bef332
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2022-05-25 16:00:37 +0300

    chore: Refactor terrafrom_fmt hook

--- hooks/terraform_fmt.sh
+++ hooks/terraform_fmt.sh
@@ -15,62 +15,7 @@ function main {
   common::parse_cmdline "$@"
   common::parse_and_export_env_vars
   # shellcheck disable=SC2153 # False positive
-  terraform_fmt_ "${ARGS[*]}" "${FILES[@]}"
-}
-
-#######################################################################
-# Hook execution boilerplate logic which is common to hooks, that run
-# on per dir basis. Little bit extended than `common::per_dir_hook`
-# 1. Because hook runs on whole dir, reduce file paths to uniq dir paths
-# (unique) 1.1. Collect paths to *.tfvars files in a separate variable
-# 2. Run for each dir `per_dir_hook_unique_part`, on all paths
-# (unique) 2.1. Run `terraform fmt` on each *.tfvars file
-# 2.2. If at least 1 check failed - change exit code to non-zero
-# 3. Complete hook execution and return exit code
-# Arguments:
-#   args (string with array) arguments that configure wrapped tool behavior
-#   files (array) filenames to check
-#######################################################################
-function terraform_fmt_ {
-  local -r args="$1"
-  shift 1
-  local -a -r files=("$@")
-  # consume modified files passed from pre-commit so that
-  # hook runs against only those relevant directories
-  local index=0
-  for file_with_path in "${files[@]}"; do
-    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
-
-    dir_paths[index]=$(dirname "$file_with_path")
-
-    ((index += 1))
-  done
-
-  # preserve errexit status
-  shopt -qo errexit && ERREXIT_IS_SET=true
-  # allow hook to continue if exit_code is greater than 0
-  set +e
-  local final_exit_code=0
-
-  # run hook for each path
-  for dir_path in $(echo "${dir_paths[*]}" | tr ' ' '\n' | sort -u); do
-    dir_path="${dir_path//__REPLACED__SPACE__/ }"
-    pushd "$dir_path" > /dev/null || continue
-
-    per_dir_hook_unique_part "$args" "$dir_path"
-
-    local exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-      final_exit_code=$exit_code
-    fi
-
-    popd > /dev/null
-  done
-  # restore errexit if it was set before the "for" loop
-  [[ $ERREXIT_IS_SET ]] && set -e
-  # return the hook final exit_code
-  exit $final_exit_code
-
+  common::per_dir_hook "${ARGS[*]}" "$HOOK_ID" "${FILES[@]}"
 }
 
 #######################################################################
@@ -85,6 +30,7 @@ function terraform_fmt_ {
 #######################################################################
 function per_dir_hook_unique_part {
   local -r args="$1"
+  # shellcheck disable=SC2034 # Unused var.
   local -r dir_path="$2"
 
   # pass the arguments to hook
