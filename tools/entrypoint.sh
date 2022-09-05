ed40055eb25cf03dfa2b8663cd2eed6208b36a9c
John Schutz <328434+tofupup@users.noreply.github.com>
2022-09-05 17:20:17 -0500

    Remove extraneous braces from variables

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 readonly USERBASE="run"
 readonly BASHPATH="/bin/bash"
+readonly HOMEPATH="/home"
 
 function echo_error_and_exit {
   echo -e "ERROR: $@" >&2
@@ -15,7 +16,7 @@ function echo_error_and_exit {
 # could be more, so we accept any valid integers
 USERID=${USERID:-"0:0"}
 if [[ ! $USERID =~ ^[0-9]+:[0-9]+$ ]]; then
-  echo_error_and_exit "USERID environment variable invalid, format is userid:groupid.  Received: \"${USERID}\""
+  echo_error_and_exit "USERID environment variable invalid, format is userid:groupid.  Received: \"$USERID\""
 fi
 
 # separate uid and gid
@@ -28,44 +29,44 @@ gid=${USERID##*:}
 # make sure workdir and some files are readable/writable by the provided UID/GID
 # combo, otherwise will have errors when processing hooks
 wdir="$(pwd)"
-if ! su-exec "$USERID" "$BASHPATH" -c "test -w ${wdir} && test -r ${wdir}"; then
-  echo_error_and_exit "uid:gid $USERID lacks permissions to ${wdir}/"
+if ! su-exec "$USERID" "$BASHPATH" -c "test -w $wdir && test -r $wdir"; then
+  echo_error_and_exit "uid:gid $USERID lacks permissions to $wdir/"
 fi
 wdirgitindex="$wdir/.git/index"
 if ! su-exec "$USERID" "$BASHPATH" -c "test -w $wdirgitindex && test -r $wdirgitindex"; then
-  echo_error_and_exit "uid:gid $USERID cannot write to ${wdir}/.git/index"
+  echo_error_and_exit "uid:gid $USERID cannot write to $wdirgitindex"
 fi
 
 # check if group by this GID already exists, if so get the name since adduser
 # only accepts names
-if groupinfo="$(getent group "${gid}")"; then
+if groupinfo="$(getent group "$gid")"; then
   groupname="${groupinfo%%:*}"
 else
   # create group in advance in case GID is different than UID
-  groupname="${USERBASE}${gid}"
-  if ! err="$(addgroup -g "${gid}" "${groupname}" 2>&1)"; then
-    echo_error_and_exit "failed to create gid \"${gid}\" with name \"${groupname}\" command output: \"$err\""
+  groupname="$USERBASE$gid"
+  if ! err="$(addgroup -g "$gid" "$groupname" 2>&1)"; then
+    echo_error_and_exit "failed to create gid \"$gid\" with name \"$groupname\" command output: \"$err\""
   fi
 fi
 
 # check if user by this UID already exists, if so get the name since id
 # only accepts names
-if userinfo="$(getent passwd "${uid}")"; then
+if userinfo="$(getent passwd "$uid")"; then
   username="${userinfo%%:*}"
 else
-  username="${USERBASE}${uid}"
-  if ! err="$(adduser -h "/home/${username}" -s "$BASHPATH" -G "${groupname}" -D -u "${uid}" -k "${HOME}" "${username}" 2>&1)"; then
-    echo_error_and_exit "failed to create uid \"${uid}\" with name \"${username}\" and group \"${groupname}\" command output: \"$err\""
+  username="$USERBASE$uid"
+  if ! err="$(adduser -h "$HOMEPATH$username" -s "$BASHPATH" -G "$groupname" -D -u "$uid" -k "$HOME" "$username" 2>&1)"; then
+    echo_error_and_exit "failed to create uid \"$uid\" with name \"$username\" and group \"$groupname\" command output: \"$err\""
   fi
 fi
 
 # it's possible it was not in the group specified, add it
-if ! idgroupinfo="$(id -G "${username}" 2>&1)"; then
-  echo_error_and_exit "failed to get group list for username \"${username}\" command output: \"$idgroupinfo\""
+if ! idgroupinfo="$(id -G "$username" 2>&1)"; then
+  echo_error_and_exit "failed to get group list for username \"$username\" command output: \"$idgroupinfo\""
 fi
-if [[ ! " ${idgroupinfo} " =~ [:blank:]${gid}[:blank:] ]]; then
-  if ! err="$(addgroup "${username}" "${groupname}" 2>&1)"; then
-    echo_error_and_exit "failed to add user \"${username}\" to group \"${groupname}\" command output: \"${err}\""
+if [[ ! " $idgroupinfo " =~ [:blank:]${gid}[:blank:] ]]; then
+  if ! err="$(addgroup "$username" "$groupname" 2>&1)"; then
+    echo_error_and_exit "failed to add user \"$username\" to group \"$groupname\" command output: \"$err\""
   fi
 fi
 
