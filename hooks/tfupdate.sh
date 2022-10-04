d699d24c69d8f39d3e5a9b3a1d4472119ed9658e
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2022-10-04 19:11:12 +0300

    Remove crutch, add normal solution, tnx to yermulnik

--- hooks/tfupdate.sh
+++ hooks/tfupdate.sh
@@ -34,80 +34,10 @@ function per_dir_hook_unique_part {
   declare -a -r args=("$@")
 
   local expand_args=()
-  for arg in "${args[@]}"; do
-    if [[ "$arg" == *'"'* ]]; then
-      elements=($arg)
-      unset start
-      unset end
-      unset quoted_var
 
-      for i in "${!elements[@]}"; do
-
-        if [[ "${elements[i]}" =~ ^\" ]]; then
-          start=$i
-        fi
-
-        if [[ "${elements[i]}" =~ \"$ ]]; then
-          end=$i
-        fi
-      done
-
-      for i in $(seq 0 $((start - 1))); do
-        expand_args+=("${elements[i]}")
-      done
-
-      for i in $(seq "$start" "$end"); do
-        quoted_var+="${elements[i]} "
-      done
-      quoted_var2=${quoted_var#'"'}
-      quoted_var2=${quoted_var2%'" '}
-      expand_args+=("$quoted_var2")
-
-      for i in $(seq $((end + 1)) $((${#elements[@]} - 1))); do
-        expand_args+=("${elements[i]}")
-      done
-
-    elif
-      [[ "$arg" == *"'"* ]]
-    then
-      # Mostly copy-paste
-
-      elements=($arg)
-      unset start
-      unset end
-      unset quoted_var
-
-      for i in "${!elements[@]}"; do
-
-        if [[ "${elements[i]}" =~ ^\' ]]; then
-          start=$i
-        fi
-
-        if [[ "${elements[i]}" =~ \'$ ]]; then
-          end=$i
-        fi
-      done
-
-      for i in $(seq 0 $((start - 1))); do
-        expand_args+=("${elements[i]}")
-      done
-
-      for i in $(seq "$start" "$end"); do
-        quoted_var+="${elements[i]} "
-      done
-      quoted_var2=${quoted_var#"'"}
-      quoted_var2=${quoted_var2%"' "}
-      expand_args+=("$quoted_var2")
-
-      for i in $(seq $((end + 1)) $((${#elements[@]} - 1))); do
-        expand_args+=("${elements[i]}")
-      done
-
-    else
-      #
-      expand_args+=($arg)
-    fi
-  done
+  while read -r -d '' ARG; do
+    expand_args+=("$ARG")
+  done < <(echo "${args[@]}" | xargs printf '%s\0')
 
   # pass the arguments to hook
   tfupdate "${expand_args[@]}" .
@@ -124,12 +54,16 @@ function per_dir_hook_unique_part {
 #   args (string with array) arguments that configure wrapped tool behavior
 #######################################################################
 function run_hook_on_whole_repo {
-  local -r args="$1"
+  declare -a -r args=("$@")
+
+  local expand_args=()
+
+  while read -r -d '' ARG; do
+    expand_args+=("$ARG")
+  done < <(echo "${args[@]}" | xargs printf '%s\0')
+
   # pass the arguments to hook
-  # shellcheck disable=SC2068 # hook fails when quoting is used ("$arg[@]")
-  # shellcheck disable=SC2048 # Use "${array[@]}" (with quotes) to prevent whitespace problems.
-  # shellcheck disable=SC2086 #  Double quote to prevent globbing and word splitting.
-  tfupdate ${args[*]} --recursive .
+  tfupdate "${expand_args[@]}" .
 
   # return exit code to common::per_dir_hook
   local exit_code=$?
