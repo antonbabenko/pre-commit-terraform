819d913e4ece883584b30061cc782edfff5688f9
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2024-02-08 22:09:39 +0200

    If plugin_cache_dir specified - lock it to avoid bug

--- hooks/_common.sh
+++ hooks/_common.sh
@@ -346,10 +346,10 @@ function common::terraform_init {
   fi
 
   # https://github.com/hashicorp/terraform/issues/31964
-  # if [ -n "$TF_PLUGIN_CACHE_DIR" ]; then
-  # echo "DBG 1. before lock $dir_path $(date)"
-  # flock --exclusive 0 #! NOT EXIST IN MAC - https://stackoverflow.com/questions/10526651/mac-os-x-equivalent-of-linux-flock1-command
-  # fi
+  if [ -n "$TF_PLUGIN_CACHE_DIR" ]; then
+    echo "$(date "+%s %N") DBG $dir_path: 1. flock --exclusive"
+    flock --exclusive 0 #! NOT EXIST IN MAC - https://stackoverflow.com/questions/10526651/mac-os-x-equivalent-of-linux-flock1-command
+  fi
 
   echo "$(date "+%s %N") DBG $dir_path: 2. before tf init"
 
@@ -365,10 +365,10 @@ function common::terraform_init {
     fi
   fi
 
-  # if [ -n "$TF_PLUGIN_CACHE_DIR" ]; then
-  #   flock --unlock 1 #! NOT EXIST IN MAC - https://stackoverflow.com/questions/10526651/mac-os-x-equivalent-of-linux-flock1-command
-  echo "$(date "+%s %N") DBG $dir_path: 3. after tf init"
-  # fi
+  if [ -n "$TF_PLUGIN_CACHE_DIR" ]; then
+    flock --unlock 1 #! NOT EXIST IN MAC - https://stackoverflow.com/questions/10526651/mac-os-x-equivalent-of-linux-flock1-command
+    echo "$(date "+%s %N") DBG $dir_path: 3. after tf init. flock --unlock"
+  fi
 
   return $exit_code
 }
