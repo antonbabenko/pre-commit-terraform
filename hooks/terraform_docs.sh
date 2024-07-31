e74b5465ebac79f5d7426f7906191b6f2b3c17f9
Lucas Ponce <ponce.ps@gmail.com>
2024-07-31 10:28:28 -0300

    fix: Hook terraform_docs should suppress command not found error message in case terraform binary does not exist

--- hooks/terraform_docs.sh
+++ hooks/terraform_docs.sh
@@ -47,7 +47,7 @@ function terraform_docs_ {
   IFS=";" read -r -a configs <<< "$hook_config"
 
   local hack_terraform_docs
-  hack_terraform_docs=$(terraform version | sed -n 1p | grep -c 0.12) || true
+  hack_terraform_docs=$(terraform version 2> /dev/null | sed -n 1p | grep -c 0.12) || true
 
   if [[ ! $(command -v terraform-docs) ]]; then
     echo "ERROR: terraform-docs is required by terraform_docs pre-commit hook but is not installed or in the system's PATH."
