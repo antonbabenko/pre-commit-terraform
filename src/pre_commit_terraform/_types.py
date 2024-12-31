f1d8faf3594cddb33a0cc2e64d85a6b9d606ab2b
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2024-12-31 19:07:23 +0200

    Fix mypy violations

--- src/pre_commit_terraform/_types.py
+++ src/pre_commit_terraform/_types.py
@@ -2,24 +2,24 @@
 
 from argparse import ArgumentParser
 from argparse import Namespace
-from typing import Final
 from typing import Protocol
+from typing import TypeAlias
 
 from pre_commit_terraform._structs import ReturnCode
 
-ReturnCodeType = ReturnCode | int
+ReturnCodeType: TypeAlias = ReturnCode | int
 
 
 class CLISubcommandModuleProtocol(Protocol):
     """A protocol for the subcommand-implementing module shape."""
 
-    HOOK_ID: Final[str]
+    HOOK_ID: str
     """This constant contains a CLI."""
 
     def populate_argument_parser(self, subcommand_parser: ArgumentParser) -> None:
         """Run a module hook for populating the subcommand parser."""
 
-    def invoke_cli_app(self, parsed_cli_args: Namespace) -> ReturnCodeType | int:
+    def invoke_cli_app(self, parsed_cli_args: Namespace) -> ReturnCodeType:
         """Run a module hook implementing the subcommand logic."""
         ...  # pylint: disable=unnecessary-ellipsis
 
