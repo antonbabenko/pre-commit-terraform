5d9e3cc435a98fadca95a3a26a5f7685fd71d4c9
Sviatoslav Sydorenko <webknjaz@redhat.com>
2025-01-03 03:56:33 +0100

    🧪 Make stderr interceptable
    Previously, `sys.stderr` was exposed as a module-scoped var, while
    `capsys` patches the `stderr` attribute on the `sys` module. So such
    patching did not influence the `stderr` variable in the `_cli` module
    as it remained linked to the non-patched object.
    
    With this change, the ability to inspect printing to stderr is
    recovered.

--- src/pre_commit_terraform/_cli.py
+++ src/pre_commit_terraform/_cli.py
@@ -1,6 +1,6 @@
 """Outer CLI layer of the app interface."""
 
-from sys import stderr
+import sys
 from typing import cast as cast_to
 
 from ._cli_parsing import initialize_argument_parser
@@ -30,25 +30,25 @@ def invoke_cli_app(cli_args: list[str]) -> ReturnCodeType:
     try:
         return invoke_cli_app(parsed_cli_args)
     except PreCommitTerraformExit as exit_err:
-        print(f'App exiting: {exit_err !s}', file=stderr)
+        print(f'App exiting: {exit_err !s}', file=sys.stderr)
         raise
     except PreCommitTerraformRuntimeError as unhandled_exc:
         print(
             f'App execution took an unexpected turn: {unhandled_exc !s}. '
             'Exiting...',
-            file=stderr,
+            file=sys.stderr,
         )
         return ReturnCode.ERROR
     except PreCommitTerraformBaseError as unhandled_exc:
         print(
             f'A surprising exception happened: {unhandled_exc !s}. Exiting...',
-            file=stderr,
+            file=sys.stderr,
         )
         return ReturnCode.ERROR
     except KeyboardInterrupt as ctrl_c_exc:
         print(
             f'User-initiated interrupt: {ctrl_c_exc !s}. Exiting...',
-            file=stderr,
+            file=sys.stderr,
         )
         return ReturnCode.ERROR
 
