c21a890fc47c3ce8133c6bff475c47d079b62551
Sviatoslav Sydorenko <webknjaz@redhat.com>
2025-01-03 03:56:33 +0100

    🧪 Make stderr interceptable
    Previously, `sys.stderr` was exposed as a module-scoped var, while
    `capsys` patches the `stderr` attribute on the `sys` module. So such
    patching did not influence the `stderr` variable in the `_cli` module
    as it remained linked to the non-patched object.
    
    With this change, the ability to inspect printing to stderr is
    recovered.

--- tests/pytest/_cli_test.py
+++ tests/pytest/_cli_test.py
@@ -21,24 +21,29 @@ pytestmark = pytest.mark.filterwarnings(
 
 
 @pytest.mark.parametrize(
-    'raised_error',
+    ('raised_error', 'expected_stderr'),
     (
         # pytest.param(PreCommitTerraformExit('sentinel'), 'App exiting: sentinel', id='app-exit'),
         pytest.param(
             PreCommitTerraformRuntimeError('sentinel'),
+            'App execution took an unexpected turn: sentinel. Exiting...',
             id='app-runtime-exc',
         ),
         pytest.param(
             PreCommitTerraformBaseError('sentinel'),
+            'A surprising exception happened: sentinel. Exiting...',
             id='app-base-exc',
         ),
         pytest.param(
             KeyboardInterrupt('sentinel'),
+            'User-initiated interrupt: sentinel. Exiting...',
             id='ctrl-c',
         ),
     ),
 )
 def test_known_interrupts(
+        capsys: pytest.CaptureFixture[str],
+        expected_stderr: str,
         monkeypatch: pytest.MonkeyPatch,
         raised_error: BaseException,
 ) -> None:
@@ -62,8 +67,14 @@ def test_known_interrupts(
 
     assert ReturnCode.ERROR == invoke_cli_app(['sentinel'])
 
+    captured_outputs = capsys.readouterr()
+    assert captured_outputs.err == f'{expected_stderr !s}\n'
 
-def test_app_exit(monkeypatch: pytest.MonkeyPatch) -> None:
+
+def test_app_exit(
+        capsys: pytest.CaptureFixture[str],
+        monkeypatch: pytest.MonkeyPatch,
+) -> None:
     """Check that an exit exception is re-raised."""
     class CustomCmdStub:
         CLI_SUBCOMMAND_NAME = 'sentinel'
@@ -84,3 +95,6 @@ def test_app_exit(monkeypatch: pytest.MonkeyPatch) -> None:
 
     with pytest.raises(PreCommitTerraformExit, match='^sentinel$'):
         invoke_cli_app(['sentinel'])
+
+    captured_outputs = capsys.readouterr()
+    assert captured_outputs.err == 'App exiting: sentinel\n'
