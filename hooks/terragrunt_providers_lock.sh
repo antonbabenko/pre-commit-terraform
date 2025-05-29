54468bb79590e155b38b462be44937c4809aa84e
bai <17348+bai@users.noreply.github.com>
2025-05-29 19:20:07 +0300

    fix(`terragrunt_*` hooks): Use new subcommands for terragrunt v0.78.0+ instead of deprecated ones (#901)
    ---------
    
    Co-authored-by: MaxymVlasov <MaxymVlasov@users.noreply.github.com>
    Co-authored-by: Julien Rottenberg <julien.rottenberg@disney.com>
    Co-authored-by: George L. Yermulnik <yz@yz.kiev.ua>

--- hooks/terragrunt_providers_lock.sh
+++ hooks/terragrunt_providers_lock.sh
@@ -14,6 +14,12 @@ function main {
   common::parse_and_export_env_vars
   # JFYI: terragrunt providers lock color already suppressed via PRE_COMMIT_COLOR=never
 
+  if common::terragrunt_version_ge_0.78; then
+    local -ra RUN_ALL_SUBCOMMAND=(run --all providers lock)
+  else
+    local -ra RUN_ALL_SUBCOMMAND=(run-all providers lock)
+  fi
+
   # shellcheck disable=SC2153 # False positive
   common::per_dir_hook "$HOOK_ID" "${#ARGS[@]}" "${ARGS[@]}" "${FILES[@]}"
 }
@@ -63,7 +69,7 @@ function run_hook_on_whole_repo {
   local -a -r args=("$@")
 
   # pass the arguments to hook
-  terragrunt run-all providers lock "${args[@]}"
+  terragrunt "${RUN_ALL_SUBCOMMAND[@]}" "${args[@]}"
 
   # return exit code to common::per_dir_hook
   local exit_code=$?
