33770fd66518a0bd4910f6ca210a6c20043a0abf
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2025-06-09 13:58:34 +0300

    fix WPS111

--- src/pre_commit_terraform/terraform_docs_replace.py
+++ src/pre_commit_terraform/terraform_docs_replace.py
@@ -107,8 +107,8 @@ def invoke_cli_app(parsed_cli_args: Namespace) -> ReturnCodeType:  # noqa: WPS23
             subprocess.check_call(' '.join(proc_args), shell=True)  # noqa: S602
         # PERF203 - try-except shouldn't be in a loop, but it's deprecated
         # hook, so leave as is
-        except subprocess.CalledProcessError as e:  # noqa: PERF203,WPS111
+        except subprocess.CalledProcessError as err:  # noqa: PERF203
             # T201 - Leave print statement as is, as this is deprecated hook
-            print(e)  # noqa: T201,WPS421
+            print(err)  # noqa: T201,WPS421
             retval = ReturnCode.ERROR
     return retval
