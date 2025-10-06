a74acee14bc4330b0a338f246eca30366744af14
Volodymyr Zahorniak <v.zahorniak@gmail.com>
2025-10-06 18:15:26 +0300

    fix sort command inconsistency

--- hooks/terraform_wrapper_module_for_each.sh
+++ hooks/terraform_wrapper_module_for_each.sh
@@ -321,7 +321,7 @@ EOF
 
     # Get names of module variables in all terraform files
     # shellcheck disable=SC2207
-    module_vars=($(echo "$all_tf_content" | hcledit block list | { grep "^variable\." | cut -d'.' -f 2 | sort || true; }))
+    module_vars=($(echo "$all_tf_content" | hcledit block list | { grep "^variable\." | cut -d'.' -f 2 | LC_ALL=C sort || true; }))
 
     # Get names of module outputs in all terraform files
     # shellcheck disable=SC2207
